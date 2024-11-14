355faa81a62d4d9e0b76c4604c42d7aac2d7bac9
WilliamsOmar <williams.bacat@uanl.edu.mx>
2024-11-14 12:16:35 -0600

    Added routes to add, update and delete anime from the db

--- routes/api.py
+++ routes/api.py
@@ -4,17 +4,12 @@ from models.database import mysql
 api_bp = Blueprint("api", __name__)
 
 @api_bp.route("/anime", methods=["GET"])
-<<<<<<< HEAD
 def get_anime():
-=======
-def get_items():
->>>>>>> 2ffc60fbc9ad54825868af7e6d9d77e5357d80cb
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT * FROM anime")
     items = cursor.fetchall()
     return jsonify(items)
 
-<<<<<<< HEAD
 #Add anime into the DB
 @api_bp.route("/anime", methods=["POST"])
 def add_anime():
@@ -51,18 +46,3 @@ def delete_anime(id):
     
     return jsonify({"message": "Anime deleted successfully"})
 
-=======
-
-#needs to be updated to current table scheme
-@api_bp.route("/item", methods=["POST"])
-def add_item():
-    data = request.get_json()
-    name = data['name']
-    description = data['description']
-    cursor = mysql.connection.cursor()
-    cursor.execute("INSERT INTO items (name, description) VALUES (%s, %s)", (name, description))
-    mysql.connection.commit()
-    return jsonify({"message": "Item added successfully"})
-
-# TODO: with db table scheme update existing routes and add DELETE and PUT routes. 
->>>>>>> 2ffc60fbc9ad54825868af7e6d9d77e5357d80cb
